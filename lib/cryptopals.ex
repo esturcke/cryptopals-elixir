96d98c6fb72a0d4384250ca3129e40055d67b0b7
Erik J. Sturcke <erik@carbonbox.com>
2014-09-10 08:32:01 -0400

    Solve set 1, challenge 1

--- lib/cryptopals.ex
+++ lib/cryptopals.ex
@@ -1,2 +1,7 @@
 defmodule Cryptopals do
+
+  def hex_to_base64(hex) do
+    Base.decode16!(hex, case: :mixed) |> Base.encode64
+  end
+
 end
--- test/cryptopals_test.exs
+++ test/cryptopals_test.exs
@@ -1,3 +1,10 @@
 defmodule CryptopalsTest do
   use ExUnit.Case
+
+  test "Set 1 / Challenge 1 - Convert hex to base64" do
+    hex    = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+    base64 = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+    assert base64 == Cryptopals.hex_to_base64 hex
+  end
+
 end
