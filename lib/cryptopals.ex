8631f4e10848d45819039efd68d814fa3131bddf
Erik J. Sturcke <erik@carbonbox.com>
2014-09-12 22:01:25 -0400

    Solve challenge 2

--- lib/cryptopals.ex
+++ lib/cryptopals.ex
@@ -1,7 +1,28 @@
 defmodule Cryptopals do
+  use Bitwise;
+
+  defp from_hex(hex) do
+    Base.decode16! hex, case: :lower
+  end
+
+  defp to_hex(bytes) do
+    Base.encode16 bytes, case: :lower
+  end
 
   def hex_to_base64(hex) do
-    Base.decode16!(hex, case: :mixed) |> Base.encode64
+    from_hex(hex) |> Base.encode64
+  end
+
+  defp byte_xor(b1, b2) do
+    [b1, b2]
+    |> Enum.map(&:binary.bin_to_list/1)
+    |> List.zip
+    |> Enum.map(fn {a,b} -> a ^^^ b end)
+    |> :binary.list_to_bin
+  end
+
+  def hex_xor(h1, h2) do
+    byte_xor(from_hex(h1), from_hex(h2)) |> to_hex
   end
 
 end
--- test/cryptopals_test.exs
+++ test/cryptopals_test.exs
@@ -7,4 +7,11 @@ defmodule CryptopalsTest do
     assert base64 == Cryptopals.hex_to_base64 hex
   end
 
-end
+  test "Set 1 / Challenge 2 - Fixed XOR" do
+    a = "1c0111001f010100061a024b53535009181c"
+    b = "686974207468652062756c6c277320657965"
+    c = "746865206b696420646f6e277420706c6179"
+    assert Cryptopals.hex_xor(a, b) == c
+  end
+
+end 
